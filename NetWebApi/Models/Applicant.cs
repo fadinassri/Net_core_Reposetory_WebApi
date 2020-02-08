03c26ea0451fa63c95d3763cfbe2723785290207
Fadi Nassri <30464695+fadinassri@users.noreply.github.com>
2020-02-08 08:30:27 -0400

    Add project files.

---
+++ NetWebApi/Models/Applicant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetWebApi.Models
+{
+    public class Applicant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public byte[] Resume { get; set; }
+    }
+}
---
+++ NetWebApi/Models/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetWebApi.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public string SalaryRange { get; set; }
+        public DateTime Deadline { get; set; }
+        public int MaximumApplicant { get; set; }
+
+    }
+}
